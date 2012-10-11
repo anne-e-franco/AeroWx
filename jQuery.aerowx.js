b4c43078755a0062a79cf5654001ea608f482d1a
Anne E Franco <anne.e.franco@gmail.com>
2012-10-11 01:14:28 -0400

    ajax Call now accepts variable station ID, function call moved to within this.each(function){}

--- jQuery.aerowx.js
+++ jQuery.aerowx.js
@@ -7,17 +7,17 @@
   		station: 'KAZO'
  	 };
 	var settings = $.extend(defaults, options);
-	$.ajax({
-		type: "GET",
-		url: "http://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20from%20xml%20WHERE%20url%3D%20%22http%3A%2F%2Fweather.aero%2Fdataserver_current%2Fhttpparam%3Fdatasource%3Dmetars%26requestType%3Dretrieve%26format%3Dxml%26mostRecentForEachStation%3Dconstraint%26hoursBeforeNow%3D1.25%26stationString%3DKAZO%22",
-		dataType: "xml",
-		success: parseXML
-	  });
+
+    this.each(function() {        
+		$.ajax({
+			type: "GET",
+			url: 'http://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20from%20xml%20WHERE%20url%3D%20%22http%3A%2F%2Fweather.aero%2Fdataserver_current%2Fhttpparam%3Fdatasource%3Dmetars%26requestType%3Dretrieve%26format%3Dxml%26mostRecentForEachStation%3Dconstraint%26hoursBeforeNow%3D1.25%26stationString%3D'+ settings.station +'%22',
+			dataType: "xml",
+			success: parseXML
+		  });
 	  function parseXML(xml){
 	  	//stub
 	  }
-    this.each(function() {        
-		
 		// Aerowx plugin code here
 		$('<div class="station"/>')
 		.appendTo(this)
