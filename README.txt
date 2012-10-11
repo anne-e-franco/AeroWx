70761906bd14ecc4d0e847c51e2ec8eb12e65fa2
Anne E Franco <anne.e.franco@gmail.com>
2012-10-11 09:59:52 -0400

    Code refactored, ADDS results (partially) reported in unordered list.

--- README.txt
+++ README.txt
@@ -1,6 +1,6 @@
 README
 
-Project: AeroWeather
+Project: AeroWx (Aero Weather)
 Author: Anne Franco
 version: 0.0.1
 
@@ -10,4 +10,14 @@ Mainfest:
 	jQuery.aerowx.js
 
 Overview:
-...
+AeroWx is intended to be a one-stop graphical dashboard for aeronautical weather information. 
+Will feature current conditions for the selected weather station (currently KBTL only).
+
+Interface:
+Clouds graphics which emulate actual cloud cover and ceiling height.
+Weather conditions: snow/fog/showers/thunderstorms.
+Wind 'compass' with a needle indicating wind direction and speed.
+
+
+Data Sources:
+ADDS textual data service http://weather.aero/tools/dataservices/textdataserver
--- jQuery.aerowx.js
+++ jQuery.aerowx.js
@@ -7,30 +7,71 @@
   		station: 'KAZO'
  	 };
 	var settings = $.extend(defaults, options);
-
+	
     this.each(function() {        
 		$.ajax({
 			type: "GET",
-			url: 'http://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20from%20xml%20WHERE%20url%3D%20%22http%3A%2F%2Fweather.aero%2Fdataserver_current%2Fhttpparam%3Fdatasource%3Dmetars%26requestType%3Dretrieve%26format%3Dxml%26mostRecentForEachStation%3Dconstraint%26hoursBeforeNow%3D1.25%26stationString%3D'+ settings.station +'%22',
+			url: 'http://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20from%20xml%20WHERE%20url%3D%20%22http%3A%2F%2Fweather.aero%2Fdataserver_current%2Fhttpparam%3Fdatasource%3Dmetars%26requestType%3Dretrieve%26format%3Dxml%26mostRecentForEachStation%3Dconstraint%26hoursBeforeNow%3D3%26stationString%3D'+ settings.station +'%22',
 			dataType: "xml",
-			success: parseXML
+			success: function(xml) {
+	        	console.log("Success - METAR Data retrieved from ADDS service");
+	        	$('<div id="'+settings.station+'"class="station"/>').appendTo('body');
+	        	$("<ul></ul>").appendTo('#'+settings.station);
+//	        	$(xml).find('METAR').each(function(){
+		
+			// Station ID
+				$("<li></li>").html("Station ID: " + settings.station).appendTo('#'+settings.station);
+			
+			// Time
+	  			var time = $(xml).find('observation_time').text();
+				$("<li></li>").html("Observation Time: " + time).appendTo('#'+settings.station);
+			
+			// Flight Rules 
+	  			var rule = $(xml).find('flight_category').text();
+				$("<li></li>").html("Flight Rule: " + rule).appendTo('#'+settings.station);
+			
+			// Temperature (degrees celcius)
+				var temp = $(xml).find('temp_c').text();
+				$("<li></li>").html("Temp: " + temp).appendTo('#'+settings.station);
+	  		
+	  		// Wind Direction
+	  			var windDir = $(xml).find('wind_dir_degrees').text();
+				$("<li></li>").html("Wind Direction: " + windDir).appendTo('#'+settings.station);
+			
+			// Wind Speed 
+	  			var windSpd = $(xml).find('wind_speed_kt').text();
+				$("<li></li>").html("Wind Speed: " + windSpd).appendTo('#'+settings.station);
+ 			
+ 			// Visibility 
+	  			var visibility = $(xml).find('visibility_statute_mi').text();
+				$("<li></li>").html("Visibility: " + visibility).appendTo('#'+settings.station);
+  			
+//	  			})
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log(textStatus); //error logging
+			}
 		  });
 	  function parseXML(xml){
-	  	//stub
+	  		$(xml).find('METAR').each(function(){
+	  			var time = $(this).find('observation_time').text();
+	  			$('<div/>').appendTo(this).text(time);
+	  			// Aerowx plugin code here
+				$('<div class="station"/>')
+				.appendTo(this)
+				.hide()
+				.text(settings.station)
+				.css({
+					backgroundColor:'#fefefe',
+					border: '1px solid #ccc',
+					'border-radius': 8,
+					height:200,
+					width:150
+				})
+				.fadeIn(450);
+			});
 	  }
-		// Aerowx plugin code here
-		$('<div class="station"/>')
-		.appendTo(this)
-		.hide()
-		.text(settings.station + '<br/>' )
-		.css({
-			backgroundColor:'#fefefe',
-			border: '1px solid #ccc',
-			'border-radius': 8,
-			height:200,
-			width:150
-		})
-		.fadeIn(450);
+		
     });
 	return this;
   };
